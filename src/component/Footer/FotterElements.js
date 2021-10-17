0afb3da8328b993837556eeceedf220784d40480
Juhasz Peter <b.juhaszpeter@gmail.com>
2021-10-17 18:57:47 +0200

    add FooterSection  styled components

--- src/component/Footer/FotterElements.js
+++ src/component/Footer/FotterElements.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import classes from './FooterElements.module.css';
-import { Link as LinkR } from 'react-router-dom';
+import { FooterContainer, FooterWrapper, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink, SocialMedia, SocialMediaWrapper, SocialMediaLogo, WebsiteRights, SocialMediaIcons, SocialMediaLink } from './FooterStyledComponents';
 import { FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin } from 'react-icons/fa';
 import { animateScroll as scroll } from 'react-scroll';
 
@@ -10,56 +9,56 @@ const FotterElements = () => {
   };
 
   return (
-    <div className={classes.FooterContainer}>
-      <div className={classes.FooterWrapper}>
-        <div className={classes.FooterLinksContainer}>
-          <div className={classes.FooterLinksWrapper}>
-            <div className={classes.FooterLinkItems}>
-              <h1 className={classes.FooterLinkTitle}>About Us</h1>
-              <LinkR className={classes.FooterLink} to='/'>How it works</LinkR>
-              <LinkR className={classes.FooterLink} to='/'>Testimonials</LinkR>
-              <LinkR className={classes.FooterLink} to='/'>Careers</LinkR>
-              <LinkR className={classes.FooterLink} to='/'>Investors</LinkR>
-              <LinkR className={classes.FooterLink} to='/'>Terms of service</LinkR>
-            </div>
-            <div className={classes.FooterLinkItems}>
-              <h1 className={classes.FooterLinkTitle}>Contact Us</h1>
-              <LinkR className={classes.FooterLink} to='/'>Contact</LinkR>
-              <LinkR className={classes.FooterLink} to='/'>Support</LinkR>
-              <LinkR className={classes.FooterLink} to='/'>Destination</LinkR>
-              <LinkR className={classes.FooterLink} to='/'>Sponsoreship</LinkR>
-            </div>
-            <div className={classes.FooterLinkItems}>
-              <h1 className={classes.FooterLinkTitle}>Videos</h1>
-              <LinkR className={classes.FooterLink} to='/'>Submit Video</LinkR>
-              <LinkR className={classes.FooterLink} to='/'>Ambassadors</LinkR>
-              <LinkR className={classes.FooterLink} to='/'>Agency</LinkR>
-              <LinkR className={classes.FooterLink} to='/'>Influencer</LinkR>
-            </div>
-            <div className={classes.FooterLinkItems}>
-              <h1 className={classes.FooterLinkTitle}>Social Media</h1>
-              <LinkR className={classes.FooterLink} to='/'>Instagram</LinkR>
-              <LinkR className={classes.FooterLink} to='/'>Facebook</LinkR>
-              <LinkR className={classes.FooterLink} to='/'>Youtube</LinkR>
-              <LinkR className={classes.FooterLink} to='/'>Twitter</LinkR>
-            </div>
-          </div>
-        </div>
-        <section className={classes.SocialMedia}>
-          <div className={classes.SocialMediaWrapper}>
-            <LinkR onClick={toggleHome} className={classes.SocialMediaLogo} to='/'>dolla</LinkR>
-            <small className={classes.WebsiteRights}>dolla © {new Date().getFullYear()}</small>
-            <div className={classes.SocialMediaIcons}>
-              <a className={classes.SocialMediaLink} href='/' target='_blank' aria-label='Facebook'><FaFacebook /></a>
-              <a className={classes.SocialMediaLink} href='/' target='_blank' aria-label='Instagram'><FaInstagram /></a>
-              <a className={classes.SocialMediaLink} href='/' target='_blank' aria-label='Youtube'><FaYoutube /></a>
-              <a className={classes.SocialMediaLink} href='/' target='_blank' aria-label='Twitter'><FaTwitter /></a>
-              <a className={classes.SocialMediaLink} href='/' target='_blank' aria-label='Linkedin'><FaLinkedin /></a>
-            </div>
-          </div>
-        </section>
-      </div>
-    </div>
+    <FooterContainer>
+      <FooterWrapper>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to='/'>How it works</FooterLink>
+              <FooterLink to='/'>Testimonials</FooterLink>
+              <FooterLink to='/'>Careers</FooterLink>
+              <FooterLink to='/'>Investors</FooterLink>
+              <FooterLink to='/'>Terms of service</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
+              <FooterLink to='/'>Contact</FooterLink>
+              <FooterLink to='/'>Support</FooterLink>
+              <FooterLink to='/'>Destination</FooterLink>
+              <FooterLink to='/'>Sponsoreship</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Videos</FooterLinkTitle>
+              <FooterLink to='/'>Submit Video</FooterLink>
+              <FooterLink to='/'>Ambassadors</FooterLink>
+              <FooterLink to='/'>Agency</FooterLink>
+              <FooterLink to='/'>Influencer</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink to='/'>Instagram</FooterLink>
+              <FooterLink to='/'>Facebook</FooterLink>
+              <FooterLink to='/'>Youtube</FooterLink>
+              <FooterLink to='/'>Twitter</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrapper>
+            <SocialMediaLogo onClick={toggleHome} to='/'>dolla</SocialMediaLogo>
+            <WebsiteRights>dolla © {new Date().getFullYear()}</WebsiteRights>
+            <SocialMediaIcons>
+              <SocialMediaLink href='/' target='_blank' aria-label='Facebook'><FaFacebook /></SocialMediaLink>
+              <SocialMediaLink href='/' target='_blank' aria-label='Instagram'><FaInstagram /></SocialMediaLink>
+              <SocialMediaLink href='/' target='_blank' aria-label='Youtube'><FaYoutube /></SocialMediaLink>
+              <SocialMediaLink href='/' target='_blank' aria-label='Twitter'><FaTwitter /></SocialMediaLink>
+              <SocialMediaLink href='/' target='_blank' aria-label='Linkedin'><FaLinkedin /></SocialMediaLink>
+            </SocialMediaIcons>
+          </SocialMediaWrapper>
+        </SocialMedia>
+      </FooterWrapper>
+    </FooterContainer>
   );
 };
 
